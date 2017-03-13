b48176f1ffed9891b4885b96c6151cd3a20ee6d0
Nick April <napril@brandeis.edu>
2017-03-13 18:06:31 -0400

    signup in progress

--- CourseCatalog/app/controllers/users_controller.rb
+++ CourseCatalog/app/controllers/users_controller.rb
@@ -1,4 +1,43 @@
 class UsersController < ApplicationController
   def new
+  	@user = User.new
   end
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        log_in @user
+        flash[:success] = "User was successfully created."
+        # redirect_to @user
+        format.html { redirect_to @user}
+        format.json { render :show, status: :created, location: @user }
+      else
+        # render 'new'
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :username, :email, :password, :password_confirmation)
+    end  
+
 end
--- CourseCatalog/config/routes.rb
+++ CourseCatalog/config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+  resources :users
+
+  get 'users/index'
+
+  get 'home/index'
+
   get 'users/new'
+  root 'home#index'
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
