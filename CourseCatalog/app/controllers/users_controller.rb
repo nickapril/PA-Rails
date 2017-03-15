1203757f015c1dad2a2eed805178d9c3624846e3
Nick April <napril@brandeis.edu>
2017-03-14 20:55:17 -0400

    working login

--- CourseCatalog/app/controllers/users_controller.rb
+++ CourseCatalog/app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
   def new
   	@user = User.new
   end
@@ -28,7 +30,30 @@ class UsersController < ApplicationController
       end
     end
   end
+  # DELETE /users/1
+  # DELETE /users/1.json
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
 
+  # PATCH/PUT /users/1
+  # PATCH/PUT /users/1.json
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+    
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
@@ -37,7 +62,7 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :username, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end  
 
 end
--- CourseCatalog/db/migrate/20170313005102_create_users.rb
+++ CourseCatalog/db/migrate/20170313005102_create_users.rb
@@ -1,7 +1,5 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     add_index :users, :email, unique: true
-
-    end
   end
 end
